21cc99c99cceaf5f963c71e714f85b3f43c5c9a8
Nazarenko Yurii <wallpapeforpc@gmail.com>
2023-04-04 20:43:35 +0300

    Added Movies and Mutations for User

--- server/FakeData.js
+++ server/FakeData.js
@@ -59,6 +59,33 @@ const  UserList = [
     age: 5,
     nationality: "CHILE",
   },
-]
+];
 
-module.exports = { UserList };
+const MovieList = [
+  {
+    id: 1,
+    name: "Avengers Endgame",
+    yearOfPublication: 2019,
+    isInTheaters: true,
+  },
+  {
+    id: 2,
+    name: "Interstellar",
+    yearOfPublication: 2007,
+    isInTheaters: true,
+  },
+  {
+    id: 3,
+    name: "Superbad",
+    yearOfPublication: 2009,
+    isInTheaters: true,
+  },
+  {
+    id: 4,
+    name: "PedroTech The Movie",
+    yearOfPublication: 2035,
+    isInTheaters: false,
+  },
+];
+
+module.exports = { UserList, MovieList };
